cebb14de068fab7eb20e10add894faac9efc2244
anjali <anjaliahlawat>
2021-03-22 21:56:19 +0530

    timeout added [#26]

--- src/tests/TestBase.js
+++ src/tests/TestBase.js
@@ -1,12 +1,15 @@
 const puppeteer = require('puppeteer');
 const { v4: uniqueId } = require('uuid');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 module.exports = class TestBase {
   constructor(){
     this.browser = ""
   }
   async createPage(){
-    this.browser = await puppeteer.launch();     
+    this.browser = await puppeteer.launch({timeout : process.env.PUPPETEER_TIMEOUT});     
     return await this.browser.newPage();
   }  
   async cleanup(){
