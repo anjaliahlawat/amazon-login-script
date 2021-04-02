151ff51bd232995cdbd1368020f3c0dd10ed018d
anjali <anjaliahlawat>
2021-04-02 18:22:13 +0530

    fix: print env var

--- src/tests/amazon/pages/AmazonLoginPage.ts
+++ src/tests/amazon/pages/AmazonLoginPage.ts
@@ -26,12 +26,10 @@ export default class AmazonLoginPage {
   }
 
   async setUsername(): Promise<void> {
-    console.log(process.env.AMAZON_USERNAME!);
     await this.page.type("#ap_email", process.env.AMAZON_USERNAME!);
   }
 
   async setPassword(): Promise<void> {
-    console.log(process.env.AMAZON_PASSWORD!);
     await this.page.type("#ap_password", process.env.AMAZON_PASSWORD!);
   }
 
